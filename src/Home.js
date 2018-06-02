eddc4d112312c24a341e14e921a610ea5f2e9396
Natividad Alvarez <tivialvarezs@gmail.com>
2018-06-02 16:42:31 -0400

    Implement basic routing

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,17 @@
-import LogSign from './components/LogSign';
 import React, { Component } from 'react';
-import Step from './components/Step';
-import './css/Home.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import {connect} from 'react-redux';
+//components
+import LandingPage from './components/LandingPage';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-      <header>
-        <div className="left">
-          <h1>Website Name</h1>
-          <p><img src="https://www.cricketwireless.com/content/dam/a/product/DAP/DAPW4105/DAPW4105-list.png"/>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui deserunt dignissimos at provident sunt ipsam quisquam iste harum laboriosam delectus cumque reprehenderit quaerat, fugiat debitis, assumenda nesciunt itaque nam!</p>
-          <button className="start">Start Now!</button>
-        </div>
-        <div className="right">
-          <LogSign/>
-        </div>
-      </header>
-      <button className="howitworks">How It Works <i className="fa fa-chevron-down"></i></button>
-      <div className="steps" id="steps">
-        <Step step="Step 1" desc="Some text describing this step" img = "http://burningrivercreative.com/imgs/analytics-laptop.png"/>
-        <Step step="Step 2" desc="Some text describing this step" img = "http://burningrivercreative.com/imgs/analytics-laptop.png"/>
-        <Step step="Step 3" desc="Some text describing this step" img = "http://burningrivercreative.com/imgs/analytics-laptop.png"/>
-      </div>
-      <footer>
-        Footer Stuff &copy;
-      </footer>
-      </div>
+      <Router>
+        <Route exact path="/" component={LandingPage}/>
+      </Router>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    auth: state.auth
-  }
-}
-
-export default connect(mapStateToProps)(Home);
+export default Home;

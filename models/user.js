5d8327d9c571c2daf5285126070b8af36c0ad26f
dmagee15 <david.magee15@yahoo.com>
2018-06-15 15:48:55 -0600

    added basic user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: 'String', required: true, unique: true },
+  password: { type: 'String', required: true },
+  schedules: Array,
+  email: { type: 'String', required: true },
+  creationDate: { type: 'Date', default: Date.now, required: true },
+});
+
+export default mongoose.model('User', userSchema);
